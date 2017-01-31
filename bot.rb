d20bf4f31a173d7c62fc8d2d34339029b0fe0335
Jack Li <jack.teng.li@gmail.com>
2017-01-31 16:22:09 -0500

    remove unnecessary stuff

--- bot.rb
+++ bot.rb
@@ -1,11 +1,9 @@
 require 'slack-ruby-bot'
-require './gravatar_helper'
 
 SlackRubyBot::Client.logger.level = Logger::ERROR
-ENV['SLACK_API_TOKEN'] = "xoxb-134333051792-AsZvXueP4h0oiojYEZCz0loF"
+ENV['SLACK_API_TOKEN'] = "<YOUR SLACK TOKEN HERE>"
 
 class Bot < SlackRubyBot::Bot
-  include GravatarHelper
 
   help do
     title 'Weather Bot'
@@ -22,42 +20,10 @@ class Bot < SlackRubyBot::Bot
     end
   end
 
-  match(/^How is the weather in (?<location>\w*)\?$/i) do |client, data, match|
+  match(/^How is the weather in (?<location>\w*)\?$/) do |client, data, match|
     client.say(channel: data.channel, text: "The weather in #{match[:location]} is nice.")
   end
 
-  match(/^Hi/) do |client, data, match|
-    puts "ok"
-
-    client.web_client.chat_postMessage(
-      channel: data.channel,
-      text: "Hello to you too",
-      attachments: [
-        {
-          author_icon: GravatarHelper::gravatar_url("jack.li@shopify.com"),
-          fallback: "fallback",
-          title: "Title",
-          text: "This is some text #{GravatarHelper::gravatar_url("jack.li@shopify.com")}",
-          color: "#95BE46"
-        }
-      ]
-    )
-  end
-
-  match(/^[Gg]ravatar for (?<email>.*@.*\..*)\??/) do |client, data, match|
-    puts "ok"
-    puts match[:email]
-    client.web_client.chat_postMessage(
-      channel: data.channel,
-      text: "Here is the gravatar image for \`#{match[:email]}\`",
-      attachments: [
-        {
-          fallback: "fallback",
-          image_url: GravatarHelper::gravatar_url(match[:email])
-        }
-      ]
-    )
-  end
 end
 
 Bot.run
